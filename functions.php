0231b3a3d33586bf7c3517ab38f0232350be17e3
Joshua Cintolo <joshua.cintolo@christianpost.com>
2021-01-14 16:18:04 -0500

    Updating database entry to be generic for local.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 	session_start();
 
-	$db = new mysqli("localhost", "joshubg0_admin", "CDAStudent", "joshubg0_thursday");
+	$db = new mysqli("localhost", "root", "root", "joshubg0_thursday");
 
 	if($db->connect_errno){
 		echo "We've got a problem: " . $db->connect_error;
